010da6a967425c133597064345fea94b5a7c3d98
Mehaavarshinicodes <mehaavarshini@gmail.com>
2026-07-26 17:58:37 +0530

    data successfully displayed in html

--- app.py
+++ app.py
@@ -9,12 +9,32 @@ def index():
 
 @app.route("/details",methods=["POST"])
 def details():
+    #setting things for api call
     city_name=request.form.get("city")
     api_key="5de2692ecc9acf75d671cc4f3f94fda9"
     url= f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+
+    #calling the api
     response=requests.get(url)
+
+    #processing the data
     data=response.json()
-    return render_template("details.html",data=data)
+    weather_description = data["weather"][0]["description"]
+    temp = data["main"]["temp"]
+    feels_like = data["main"]["feels_like"]
+    temp_min = data["main"]["temp_min"]
+    temp_max = data["main"]["temp_max"]
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    return render_template("details.html",
+                           city_name=city_name,
+                           weather_description=weather_description,
+                           temp=temp,
+                           feels_like=feels_like,
+                           temp_min=temp_min,
+                           temp_max=temp_max,
+                           pressure=pressure,
+                           humidity=humidity)
 
 if __name__ == "__main__":
    app.run(debug=True)
